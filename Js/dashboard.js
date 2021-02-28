a51b30b2379c3d31db3268adfc0e6f70096fb6a6
Dhanashri Sakharkar <dhanashrisakharkar333@gmail.com>
2021-03-01 00:49:55 +0530

    [Dhanashri] Refactor button chnging colour and lenght after clicking on it and whistle button get hide

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -29,7 +29,7 @@ const displayBooks = () => {
                 <div style=" width: 300px;font-size: 14px;  border: none; margin:5px 5px 10px 0px">${details.quantity}</div>
                 <div style=" width: 300px;font-size: 14px;    border: none;  margin:-7px 5px 15px 0px">${details.price}</div>
                 <p style="display:none" class="card-text" style=" margin:5px 5px 15px 25px">${details.author}</p>
-                <button type="button" class="btn1 btn-danger" style="left: 20px; margin: 0% 0% 5% -5%; position: inherit;" id=${details._id} onclick='addToCart(this.id) '  >Add to cart</button>
+                <button type="button" class="btn1 btn-danger" style="left: 20px; margin: 0% 0% 5% -5%; position: inherit;" id=${details._id} onclick='addToCart(this.id) ,style.backgroundColor = "#3371B5",style.width="80%",document.getElementById("whistle").style.display="none"'  >Add to cart</button>
                 <button type="button" id="whistle" class="btn1 btn-danger" style=" width:50%; background-color: white; color:black  ; border-color:whitesmoke; margin: 0% 0% 5% 4%; position: absolute;"  >Wishlist</button>
             </div>
     </div>

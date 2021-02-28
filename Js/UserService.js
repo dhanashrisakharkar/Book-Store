4cd62a072397fc52cbbba659c2a8350ed812a53f
Dhanashri Sakharkar <dhanashrisakharkar333@gmail.com>
2021-03-01 00:59:25 +0530

    [Dhanashri] Refactor. added api in object to call ir add to cart api

---
+++ Js/UserService.js
@@ -0,0 +1,7 @@
+let apiCall = {
+    signUp : "https://backend-bookstore.herokuapp.com/bookstore_user/registration",
+    login : "https://backend-bookstore.herokuapp.com/bookstore_user/login",
+    bookDisplay : "https://backend-bookstore.herokuapp.com/bookstore_user/get/book",
+    addToCart : "https://backend-bookstore.herokuapp.com/bookstore_user/add_cart_item",
+   
+}

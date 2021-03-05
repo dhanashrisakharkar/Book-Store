769d5a1c16b73e30250a5f3c1b35e172defca100
Dhanashri Sakharkar <dhanashrisakharkar333@gmail.com>
2021-03-06 00:38:44 +0530

    [Dhanashri] Refactor. added validation to the customer form details

--- Js/formValidation.js
+++ Js/formValidation.js
@@ -2,9 +2,6 @@ let validate =(event , id) => {
     const fname = document.getElementById('fname').value;
     const phone = document.getElementById('phone').value;
     const pincode = document.getElementById('pincode').value;
-    // const textError = document.getElementById('.text-error').value;
-    // const textErrorTwo = document.getElementById('.phone-error').value;
-    // const textErrorThree = document.getElementById('.pincode-error').value;
     let fnameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
     let txt = ""
     if (fname === fnameRegex )
@@ -15,45 +12,45 @@ let validate =(event , id) => {
         document.getElementById('.text-error').innerHTML = txt;
     }
     
-    // fname.addEventListener('input', function () {
-    //     if (fname.value.length == 0) {
-    //         textError.textContent = "";
-    //         return;
-    //     }
-    //     try {
-    //         (new SignUpData()).fname = fname.value;
-    //         textError.textContent = "";
-    //     } catch (e) {
-    //         textError.textContent = e;
-    //     }
-    // })
+    fname.addEventListener('input', function () {
+        if (fname.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new SignUpData()).fname = fname.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    })
 
     
-    // phone.addEventListener('input', function () {
-    //     if (phone.value.length == 0) {
-    //         textErrorTwo.textContent = "";
-    //         return;
-    //     }
-    //     try {
-    //         (new SignUpData()).phone = phone.value;
-    //         textErrorTwo.textContent = "";
-    //     } catch (e) {
-    //         textErrorTwo.textContent = e;
-    //     }
-    // })
+    phone.addEventListener('input', function () {
+        if (phone.value.length == 0) {
+            textErrorTwo.textContent = "";
+            return;
+        }
+        try {
+            (new SignUpData()).phone = phone.value;
+            textErrorTwo.textContent = "";
+        } catch (e) {
+            textErrorTwo.textContent = e;
+        }
+    })
 
-    // pincode.addEventListener('input', function () {
-    //     if (pincode.value.length == 0) {
-    //         textErrorThree.textContent = "";
-    //         return;
-    //     }
-    //     try {
-    //         (new SignUpData()).pincode = pincode.value;
-    //         textErrorThree.textContent = "";
-    //     } catch (e) {
-    //         textErrorThree.textContent = e;
-    //     }
-    // })
+    pincode.addEventListener('input', function () {
+        if (pincode.value.length == 0) {
+            textErrorThree.textContent = "";
+            return;
+        }
+        try {
+            (new SignUpData()).pincode = pincode.value;
+            textErrorThree.textContent = "";
+        } catch (e) {
+            textErrorThree.textContent = e;
+        }
+    })
     
 
 };
